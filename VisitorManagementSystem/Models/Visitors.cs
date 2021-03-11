206c8a3624d9f68305e82b88cb3d15c42133430c
Karandeep Singh <sandhukarn11@gmail.com>
2021-03-11 14:23:18 +1300

    Add project files.

---
+++ VisitorManagementSystem/Models/Visitors.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisitorManagementSystem.Models
+{
+
+    [Authorize]
+    public class Visitors
+        {
+            [Key]
+        public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Business { get; set; }
+            public DateTime DateIn { get; set; }
+            public DateTime DateOut { get; set; }
+             public StaffNames StaffName { get; set; }
+        }
+     
+    
+}
